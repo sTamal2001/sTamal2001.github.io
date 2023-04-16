67d7c51a82fec1759cb03ef3462ec70dcb586a81
Tamal Sen <102846261+sTamal2001@users.noreply.github.com>
2023-04-16 11:57:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navebar = document.querySelector(".navebar");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navebar.classList.toggle("active");
+};
+
 // Scrool section active link
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
@@ -17,4 +25,30 @@ window.onscroll = () => {
       });
     }
   });
+
+  // Sticky navebar
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and naveabr when click navebar link (scrool)
+  menuIcon.classList.remove("bx-x");
+  navebar.classList.remove("active");
 };
+
+// Scroll Revael
+ScrollReveal({ reset: true, distance: "80px", duration: 2000, delay: 200 });
+ScrollReveal().reveal(".home-c,.heading", { origin: "top" });
+ScrollReveal().reveal(".home-img, .service-c, .protfolio-b, .contact form", {
+  origin: "bottom",
+});
+ScrollReveal().reveal(".home-c h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-c p, .about-c", { origin: "right" });
+
+// Typed JS
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Backend Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
